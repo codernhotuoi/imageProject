8a6a6eb5d65d8e0e7e3d633e65fc3829e684faf1
hongnm2202 <94152885+hongnm2202@users.noreply.github.com>
2023-05-22 15:45:34 +0700

    feat image project

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+import Image from './src/Image';
+
+AppRegistry.registerComponent(appName, () => Image);
